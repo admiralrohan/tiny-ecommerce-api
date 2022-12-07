c5f3bd7aed68d0e7fb94cb245435ca8d37e469f1
Rohan Gayen <rohangayen@gmail.com>
2022-12-08 01:35:56 +0530

    feat: `/user-roles` endpoint done

--- routes/utils.ts
+++ routes/utils.ts
@@ -1,10 +1,15 @@
 import { Request, Response, Router } from "express";
+import { userTypes } from "../configs/constants";
 
 const router = Router();
 
 // To show at registration form at FE
 router.get("/user-roles", (req: Request, res: Response) => {
-  res.json({ success: true, message: "Fetched user roles successfully" });
+  res.json({
+    success: true,
+    message: "Fetched user roles successfully",
+    data: userTypes,
+  });
 });
 
 router.get("/products", (req: Request, res: Response) => {
