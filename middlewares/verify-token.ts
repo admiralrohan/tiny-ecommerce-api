891ff0e616159314c8e8883784b3505b0684cf12
Rohan Gayen <rohangayen@gmail.com>
2022-12-07 13:36:00 +0530

    feat: verify user provided token through DB entry
    Whether the token is used in past and that entry is logged out already.

--- middlewares/verify-token.ts
+++ middlewares/verify-token.ts
@@ -1,22 +1,40 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { jwtSecret } from "../configs/config";
+import Tokens from "../models/tokens";
 
 export async function verifyToken(
   req: Request,
   res: Response,
   next: NextFunction
 ) {
-  if (!req.header("Authorization")) throw new Error("Auth token missing");
-  const token = req.header("Authorization")?.replace("Bearer ", "");
+  try {
+    if (!req.header("Authorization")) throw new Error("Auth token missing");
+    const token = req.header("Authorization")?.replace("Bearer ", "");
 
-  if (!token) throw new Error("Token is required");
-  if (!jwtSecret) throw new Error("JWT Secret is required");
-  const decoded = jwt.verify(token, jwtSecret);
-  const userId = (decoded as any).id;
+    if (!token) throw new Error("Token is required");
+    if (!jwtSecret) throw new Error("JWT Secret is required");
+    const decoded = jwt.verify(token, jwtSecret);
+    const userId = (decoded as any).id;
 
-  (res as any).userId = userId;
-  (res as any).token = token;
+    (res as any).userId = userId;
+    (res as any).token = token;
 
-  next();
+    const result = await Tokens.query().findOne({
+      userId,
+      token,
+      loggedOutAt: null,
+    });
+    if (!result) throw new Error("Token not found");
+
+    next();
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(401).json({
+        success: false,
+        message: "Authentication required",
+        error: error.message,
+      });
+    }
+  }
 }
--- models/tokens.ts
+++ models/tokens.ts
@@ -3,6 +3,8 @@ import { Model } from "objection";
 export default class Tokens extends Model {
   static tableName = "tokens";
 
+  /** Token Id doesn't matter for any verifications, just used for Primary key,
+   * Maybe consider using actual token as primary key.  */
   id!: number;
   userId!: number;
   token!: string;
