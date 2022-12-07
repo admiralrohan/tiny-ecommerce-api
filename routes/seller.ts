55b8ce3e9b1a172c85c67a75986715eeaacbf0fe
Rohan Gayen <rohangayen@gmail.com>
2022-12-08 03:54:20 +0530

    feat: `/create-catalog` route

--- routes/seller.ts
+++ routes/seller.ts
@@ -1,9 +1,32 @@
 import { Request, Response, Router } from "express";
+import Users from "../models/users";
 
 const router = Router();
 
-router.post("/create-catalog", (req: Request, res: Response) => {
-  res.json({ success: true, message: "Creation of seller catalog successful" });
+router.post("/create-catalog", async (req: Request, res: Response) => {
+  try {
+    const { products } = req.body;
+
+    // TODO: Check if all products belong to the seller
+    const insertedResult = await Users.query().findById(res.userId).patch({
+      catalog: products,
+    });
+
+    if (!insertedResult) throw new Error("DB insertion failed");
+
+    res.json({
+      success: true,
+      message: "Created seller catalog",
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(400).json({
+        success: false,
+        message: "Couldn't create new catalog",
+        error: error.message,
+      });
+    }
+  }
 });
 
 router.get("/orders", (req: Request, res: Response) => {
