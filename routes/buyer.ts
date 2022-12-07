2afe490b3007c6daa49818cc9ad0cc92c6502a1d
Rohan Gayen <rohangayen@gmail.com>
2022-12-07 22:55:04 +0530

    feat: restrict `/list-of-sellers` route to buyers only

--- routes/buyer.ts
+++ routes/buyer.ts
@@ -1,31 +1,39 @@
 import { Request, Response, Router } from "express";
+import { verifyToken } from "../middlewares/verify-token";
 import Users from "../models/users";
 
 const router = Router();
 
-router.get("/list-of-sellers", async (req: Request, res: Response) => {
-  try {
-    const listOfSellers = await Users.query()
-      .select("id", "username", "email")
-      .where({ type: "seller" });
+router.get(
+  "/list-of-sellers",
+  verifyToken,
+  async (req: Request, res: Response) => {
+    try {
+      if ((res as any).userType !== "buyer")
+        throw new Error("Route is only available for buyers");
 
-    res.json({
-      success: true,
-      message: "Fetched list of sellers successfully",
-      data: {
-        listOfSellers,
-      },
-    });
-  } catch (error) {
-    if (error instanceof Error) {
-      res.status(400).json({
-        success: false,
-        message: "Couldn't fetch list of sellers",
-        error: error.message,
+      const listOfSellers = await Users.query()
+        .select("id", "username", "email")
+        .where({ type: "seller" });
+
+      res.json({
+        success: true,
+        message: "Fetched list of sellers successfully",
+        data: {
+          listOfSellers,
+        },
       });
+    } catch (error) {
+      if (error instanceof Error) {
+        res.status(400).json({
+          success: false,
+          message: "Couldn't fetch list of sellers",
+          error: error.message,
+        });
+      }
     }
   }
-});
+);
 
 router.get("/seller-catalog/:seller_id", (req: Request, res: Response) => {
   res.json({ success: true, message: "Fetched seller catalog successfully" });
