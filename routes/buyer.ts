ebd553cb697c89c59d77bbbb2a82b36489e531f7
Rohan Gayen <rohangayen@gmail.com>
2022-12-08 06:12:06 +0530

    feat: get catalog by `seller_id`

--- routes/buyer.ts
+++ routes/buyer.ts
@@ -1,4 +1,5 @@
 import { Request, Response, Router } from "express";
+import Products from "../models/product";
 import Users from "../models/users";
 
 const router = Router();
@@ -27,11 +28,46 @@ router.get("/list-of-sellers", async (req: Request, res: Response) => {
   }
 });
 
-router.get("/seller-catalog/:seller_id", (req: Request, res: Response) => {
-  res.json({ success: true, message: "Fetched seller catalog successfully" });
-});
+/**
+ * Return product IDs
+ */
+router.get(
+  "/seller-catalog/:seller_id",
+  async (req: Request, res: Response) => {
+    try {
+      const { seller_id: sellerId } = req.params;
+      const userDetails = await Users.query()
+        .select("catalog", "type")
+        .findById(sellerId);
+
+      if (!userDetails) throw new Error("User not found");
+
+      const { catalog: productIds, type: userType } = userDetails;
+      if (userType !== "seller") throw new Error("User is not a seller");
+
+      const productList = await Products.query().whereIn("id", productIds);
+
+      res.json({
+        success: true,
+        message: "Fetched seller catalog successfully",
+        data: {
+          catalog: productList,
+        },
+      });
+    } catch (error) {
+      if (error instanceof Error) {
+        res.status(400).json({
+          success: false,
+          message: "Couldn't fetch catalog",
+          error: error.message,
+        });
+      }
+    }
+  }
+);
 
 router.post("/create-order/:seller_id", (req: Request, res: Response) => {
+  // TODO: Exclude own products
   res.json({ success: true, message: "Order creation successful" });
 });
 
