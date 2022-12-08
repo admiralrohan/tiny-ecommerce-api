9e35370312da4b84762ce3419d17817da48fce26
Rohan Gayen <rohangayen@gmail.com>
2022-12-08 21:54:13 +0530

    test: non-db tests
    Root route
    404
    /user-roles

---
+++ __tests__/basic.test.ts
@@ -0,0 +1,43 @@
+import request from "supertest";
+import { app, server } from "../app";
+import { userTypes } from "../configs/constants";
+
+afterAll(() => {
+  server.close();
+});
+
+describe("GET /", function () {
+  it("responds with json", async function () {
+    const response = await request(app)
+      .get("/")
+      .set("Accept", "application/json");
+
+    expect(response.headers["content-type"]).toMatch(/json/i);
+    expect(response.status).toEqual(200);
+    expect(response.body.success).toBeTruthy();
+  });
+});
+
+describe("GET /404", function () {
+  it("responds with json", async function () {
+    const response = await request(app)
+      .get("/random")
+      .set("Accept", "application/json");
+
+    expect(response.headers["content-type"]).toMatch(/json/i);
+    expect(response.status).toEqual(404);
+    expect(response.body.success).toBeFalsy();
+  });
+});
+
+describe("GET /api/utils/user-roles", function () {
+  it("responds with json", async function () {
+    const response = await request(app)
+      .get("/api/utils/user-roles")
+      .set("Accept", "application/json");
+
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.status).toEqual(200);
+    expect(response.body.data).toEqual(userTypes);
+  });
+});
