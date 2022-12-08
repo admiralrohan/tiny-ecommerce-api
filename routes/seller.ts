c12c48fafd65f9d7276cd1252399c48905c39694
Rohan Gayen <rohangayen@gmail.com>
2022-12-08 07:42:12 +0530

    feat: `/seller/orders` route

--- routes/seller.ts
+++ routes/seller.ts
@@ -1,4 +1,5 @@
 import { Request, Response, Router } from "express";
+import Orders from "../models/order";
 import Products from "../models/product";
 import Users from "../models/users";
 
@@ -37,11 +38,73 @@ router.post("/create-catalog", async (req: Request, res: Response) => {
   }
 });
 
-router.get("/orders", (req: Request, res: Response) => {
-  res.json({
-    success: true,
-    message: "Fetched list of received orders successfully",
-  });
+router.get("/orders", async (req: Request, res: Response) => {
+  /** Attach properties from `Users` */
+  interface OrderPlus extends Orders {
+    buyerName: string;
+    buyerEmail: string;
+  }
+
+  try {
+    const ordersResponse = (await Orders.query()
+      .alias("o")
+      .join("users as u", "u.id", "=", "o.buyerId")
+      .select(
+        "o.id",
+        "o.buyerId",
+        "u.email as buyerEmail",
+        "u.username as buyerName",
+        "o.productIds",
+        "o.createdAt",
+        "o.completedAt"
+      )) as OrderPlus[];
+
+    // Find unique productIDs
+    const uniqueProductIds = new Set<number>();
+    ordersResponse.forEach((order) => {
+      order.productIds.forEach((productId) => {
+        uniqueProductIds.add(productId);
+      });
+    });
+
+    // Remove "ownerId", redundant info
+    const productList = await Products.query()
+      .select("id", "name", "price", "isActive", "createdAt")
+      .whereIn("id", Array.from(uniqueProductIds));
+
+    // Construct the final response
+    const orders = ordersResponse.map((order) => {
+      return {
+        id: order.id,
+        buyerId: order.buyerId,
+        buyerName: order.buyerName,
+        buyerEmail: order.buyerEmail,
+        // Ideally maps are better in these scenarios than array traverse,
+        // but here the no of products shouldn't be too many
+        products: order.productIds.map((productId) =>
+          productList.find((productDetail) => productDetail.id === productId)
+        ),
+        createdAt: order.createdAt,
+        completedAt: order.completedAt,
+      };
+    });
+
+    res.json({
+      success: true,
+      message: "Fetched list of received orders successfully",
+      data: {
+        orders,
+      },
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(400).json({
+        success: false,
+        message: "Couldn't fetch list of orders",
+        error: error.message,
+      });
+    }
+  }
 });
 
 export default router;
