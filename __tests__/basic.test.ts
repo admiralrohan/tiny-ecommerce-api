bc0d67b9236f06525dea9219f0f030cf9073c2c1
Rohan Gayen <rohangayen@gmail.com>
2022-12-09 12:48:59 +0530

    refactor: change test titles + change to arrow function

--- __tests__/basic.test.ts
+++ __tests__/basic.test.ts
@@ -2,8 +2,8 @@ import request from "supertest";
 import app from "../app";
 import { userTypes } from "../configs/constants";
 
-describe("GET /", function () {
-  it("responds with json", async function () {
+describe("GET /", () => {
+  it("responds properly", async () => {
     const response = await request(app)
       .get("/")
       .set("Accept", "application/json");
@@ -14,8 +14,8 @@ describe("GET /", function () {
   });
 });
 
-describe("GET /404", function () {
-  it("responds with json", async function () {
+describe("GET /404", () => {
+  it("responds properly", async () => {
     const response = await request(app)
       .get("/random")
       .set("Accept", "application/json");
@@ -26,8 +26,8 @@ describe("GET /404", function () {
   });
 });
 
-describe("GET /api/utils/user-roles", function () {
-  it("responds with json", async function () {
+describe("GET /api/utils/user-roles", () => {
+  it("responds properly", async () => {
     const response = await request(app)
       .get("/api/utils/user-roles")
       .set("Accept", "application/json");
